44558c98d59979996bcd9dc025b0d6749bf6749e
prakashsinha271 <prakashsinha271@gmail.com>
2021-01-15 13:05:26 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_api_demo/view.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _APIDemo createState() => _APIDemo();
+}
+
+class _APIDemo extends State<MyApp> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController mobileController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('API Demo'),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'API Creation and Hosting\nNodeJS',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '!!Guided by Ankit Sir!!',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: TextFormField(
+                          controller: nameController,
+                          keyboardType: TextInputType.name,
+                          maxLines: 1,
+                          maxLength: 20,
+                          validator: (value){
+                            if(value == null || value == "" || value.isEmpty){
+                              return "Valid Name Required";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Full Name',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: TextFormField(
+                          obscureText: true,
+                          controller: mobileController,
+                          keyboardType: TextInputType.number,
+                          maxLines: 1,
+                          maxLength: 10,
+                          validator: (value){
+                            if(value.length < 10){
+                              return "Valid Mobile Number Required";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Mobile Number',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text('Save'),
+                      onPressed: () {
+                        setState(() {
+                          debugPrint("Save button clicked");
+                          if (_formKey.currentState.validate()) {
+                            _saveData();
+                          }
+                        });
+                      },
+                    )),
+                Container(
+                    child: Row(
+                      children: <Widget>[
+                        Text('View Saved Data'),
+                        FlatButton(
+                          textColor: Colors.blue,
+                          child: Text(
+                            'Click here',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ViewData()),
+                            );
+                          },
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ))
+              ],
+            )
+        )
+
+    );
+  }
+
+  /*
+  ============
+  Save Data into API
+  ============
+   */
+  void _saveData(){
+    debugPrint("Save button clicked");
+    debugPrint(nameController.text);
+    debugPrint(mobileController.text);
+  }
+}
---
+++ lib/view.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+enum ViewDataBy { name, number, all } //Radio Item
+
+class ViewData extends StatefulWidget {
+  ViewData({Key key}) : super(key: key);
+  @override
+  _ViewDataState createState() => _ViewDataState();
+}
+
+class _ViewDataState extends State<ViewData> {
+  ViewDataBy _mode =
+      ViewDataBy.name; //Initializing radio variable for default value
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _inputType;
+  String _radioVal;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _inputType = TextEditingController();
+    _radioVal = "name";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('View API Data'),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(15.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Radio(
+                        value: ViewDataBy.name,
+                        groupValue: _mode,
+                        onChanged: (ViewDataBy value) {
+                          setState(() {
+                            _mode = value;
+                            _radioVal = "name";
+                            _inputType.clear();
+                          });
+                        },
+                      ),
+                      Text("Name"),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Radio(
+                        value: ViewDataBy.number,
+                        groupValue: _mode,
+                        onChanged: (ViewDataBy value) {
+                          setState(() {
+                            _mode = value;
+                            _radioVal = "number";
+                            _inputType.clear();
+                          });
+                        },
+                      ),
+                      Text("Number"),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Radio(
+                        value: ViewDataBy.all,
+                        groupValue: _mode,
+                        onChanged: (ViewDataBy value) {
+                          setState(() {
+                            _mode = value;
+                            _radioVal = "all";
+                            _inputType.clear();
+                          });
+                        },
+                      ),
+                      Text("All Data"),
+                    ],
+                  ),
+                ),
+                _radioVal == "name"
+                    ? Container(
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: TextFormField(
+                          obscureText: true,
+                          controller: _inputType,
+                          keyboardType: TextInputType.text,
+                          validator: (value) {
+                            if (value == null || value == "" || value.isEmpty) {
+                              return "Valid Name Required";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Full Name',
+                          ),
+                        ),
+                      )
+                    : _radioVal == "number"
+                        ? Container(
+                            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                            child: TextFormField(
+                              obscureText: true,
+                              controller: _inputType,
+                              keyboardType: TextInputType.text,
+                              validator: (value) {
+                                if (value.length < 10) {
+                                  return "Valid Mobile Number Required";
+                                }
+                                return null;
+                              },
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Mobile Number',
+                              ),
+                            ),
+                          )
+                        : Text(""),
+                FloatingActionButton(
+                    onPressed: (){
+                      if (_formKey.currentState.validate()) {
+                        debugPrint("FormValidate");
+                        _fetchAPI();
+                      }
+                    }
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  void _fetchAPI(){
+    debugPrint(_inputType.text);
+    debugPrint(_radioVal);
+    debugPrint("Inside _fetchAPI()");
+  }
+}
